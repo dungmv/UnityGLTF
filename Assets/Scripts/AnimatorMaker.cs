d376b29567a5f2f98c825fac8d1f60f5b31d36c6
dungmv <maivandung91@gmail.com>
2023-04-13 22:19:05 +0700

    update anim

--- Assets/Scripts/AnimatorMaker.cs
+++ Assets/Scripts/AnimatorMaker.cs
@@ -1,54 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using ReadyPlayerMe.AvatarLoader;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
 using UnityEngine.UI;
+using GLTFast;
+using UnityGLTF;
+using System.IO;
+using WrapMode = UnityEngine.WrapMode;
 
 public class AnimatorMaker : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("RPM avatar URL or shortcode to load")]
+    [SerializeField] [Tooltip("RPM avatar URL or shortcode to load")]
     private string avatarUrl;
+
     private GameObject avatar;
     private AvatarObjectLoader avatarObjectLoader;
-    [SerializeField]
-    [Tooltip("Animator to use on loaded avatar")]
+
+    [SerializeField] [Tooltip("Animator to use on loaded avatar")]
     private RuntimeAnimatorController animatorController;
-    [SerializeField]
-    [Tooltip("If true it will try to load avatar from avatarUrl on start")]
+
+    [SerializeField] [Tooltip("If true it will try to load avatar from avatarUrl on start")]
     private bool loadOnStart = true;
+
     [SerializeField]
     [Tooltip("Preview avatar to display until avatar loads. Will be destroyed after new avatar is loaded")]
     private GameObject previewAvatar;
-    [SerializeField]
-    private AnimationClip[] clips;
-    [SerializeField]
-    private Animator animator;
-    [SerializeField]
-    private PlayableGraph playableGraph;
-    [SerializeField]
-    private RectTransform animationContainer;
-    [SerializeField]
-    private Button prefabItem;
 
-    private AnimationClipPlayable currentAnimation;
+    [SerializeField] private AnimationClip[] clips;
+    [SerializeField] private Animator animator;
+    [SerializeField] private RectTransform animationContainer;
+    [SerializeField] private Button prefabItem;
+    [SerializeField] private float time;
+    
+    private PlayableGraph playableGraph;
+    private AnimationClipPlayable playableClip;
 
 #if UNITY_WEBGL
     [DllImport("__Internal")]
     private static extern void AnimationSelected(int id);
+
     [DllImport("__Internal")]
     private static extern void OnAvatarLoadCompleted(string url);
+
     [DllImport("__Internal")]
     private static extern void OnAvatarLoadFailed(string url, string msg);
 #endif
 
     private void Start()
     {
+        playableGraph = PlayableGraph.Create();
+        playableGraph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
+        
         avatarObjectLoader = new AvatarObjectLoader();
         avatarObjectLoader.OnCompleted += OnLoadCompleted;
         avatarObjectLoader.OnFailed += OnLoadFailed;
@@ -57,10 +64,13 @@ public class AnimatorMaker : MonoBehaviour
         {
             SetupAvatar(previewAvatar);
         }
+
         if (loadOnStart)
         {
             LoadAvatar(avatarUrl);
         }
+        
+        PlayAnimationById(0);
 
         for (int i = 0; i < clips.Length; i++)
         {
@@ -73,15 +83,12 @@ public class AnimatorMaker : MonoBehaviour
             item.onClick.AddListener(() => { PlayAnimationById(animId); });
         }
 #if !UNITY_EDITOR && UNITY_WEBGL
-     WebGLInput.captureAllKeyboardInput = false;
+        WebGLInput.captureAllKeyboardInput = false;
 #endif
     }
 
     private void OnEnable()
     {
-        playableGraph = PlayableGraph.Create();
-        playableGraph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
-
         //TouchSimulation.Enable();
         //EnhancedTouchSupport.Enable
         //UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerMove += OnTouchMove;
@@ -92,6 +99,10 @@ public class AnimatorMaker : MonoBehaviour
         //UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerMove -= OnTouchMove;
         //TouchSimulation.Disable();
         //EnhancedTouchSupport.Disable();
+    }
+
+    private void OnDestroy()
+    {
         playableGraph.Destroy();
     }
 
@@ -109,6 +120,7 @@ public class AnimatorMaker : MonoBehaviour
             Destroy(previewAvatar);
             previewAvatar = null;
         }
+
         SetupAvatar(args.Avatar);
 #if !UNITY_EDITOR && UNITY_WEBGL
         OnAvatarLoadCompleted(args.Url);
@@ -154,9 +166,9 @@ public class AnimatorMaker : MonoBehaviour
         var playableOutput = AnimationPlayableOutput.Create(playableGraph, "AnimationClip", animator);
         // Wrap the clip in a playable
         clip.wrapMode = WrapMode.Loop;
-        var clipPlayable = AnimationClipPlayable.Create(playableGraph, clip);
+        playableClip = AnimationClipPlayable.Create(playableGraph, clip);
         // Connect the Playable to an output
-        playableOutput.SetSourcePlayable(clipPlayable); 
+        playableOutput.SetSourcePlayable(playableClip);
         // Plays the Graph.
         playableGraph.Play();
     }
@@ -169,11 +181,59 @@ public class AnimatorMaker : MonoBehaviour
     //        animator.transform.Rotate(new Vector3(0, delta, 0)); 
     //    }
     //}
-    
-    public void Combine()
+
+    async public void LoadModelFromGLTF()
     {
-        AnimationClip[] animations = AnimationUtility.GetAnimationClips(avatar);
-        var export = new GLTFast.GltfImport();
- 
+        var setting = new ImportSettings();
+        var gltf = new GltfImport();
+        bool success = await gltf.Load(avatarUrl, setting);
+        if (!success)
+        {
+            Debug.LogWarning("Can not load model");
+            return;
+        }
+        var go = new GameObject("Avatar");
+        go.transform.parent = transform;
+
+        GltFastGameObjectInstantiator customInstantiator = new GltFastGameObjectInstantiator(gltf, go.transform);
+        success = await gltf.InstantiateMainSceneAsync(customInstantiator);
+        if (!success)
+        {
+            Debug.Log("Can not instantiate model");
+            return;
+        }
+
+        if (previewAvatar != null)
+        {
+            Destroy(previewAvatar);
+            previewAvatar = null;
+        }
+
+        go.AddComponent<Animator>();
+
+        SetupAvatar(go);
+        Debug.Log("Load model completed!");
+    }
+
+    [ContextMenu("Export GLB")]
+    public void ExportAvatarWithAnimationClip()
+    {
+        // GLTFRecorder recorder = new GLTFRecorder(avatar.transform);
+        // recorder.StartRecording(0);
+        // recorder.UpdateRecording(1);
+        // recorder.EndRecording("Avatar@run.glb");
+
+        var options = new ExportOptions();
+        options.AfterSceneExport += (sceneExporter, root) =>
+        {
+            foreach (var clip in clips)
+            {
+                sceneExporter.ExportAnimationClip(clip, clip.name, avatar.transform, 1);
+            }
+        };
+        GLTFSceneExporter exporter = new GLTFSceneExporter(avatar.transform, options);
+        exporter.SaveGLB(Path.Combine(Application.dataPath, "Models"), "Avatar.glb");
+        Debug.LogFormat("Save file completed " + Path.Combine(Application.dataPath, "Models", "Avatar.glb"));
     }
 }
+
