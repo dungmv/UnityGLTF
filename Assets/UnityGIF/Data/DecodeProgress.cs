a14add485232e720f91e3b5c2699f2e0dcaed4a5
dungmv <maivandung91@gmail.com>
2023-04-18 23:32:09 +0700

    unity gif

---
+++ Assets/UnityGIF/Data/DecodeProgress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UnityGIF.Data
+{
+	public class DecodeProgress
+	{
+		public int Progress;
+		public int FrameCount;
+		public bool Completed;
+		public Gif Gif;
+		public Exception Exception;
+	}
+}
---
+++ Assets/UnityGIF/Data/GifFrame.cs
@@ -0,0 +1,20 @@
+﻿using UnityGIF.Enums;
+using UnityGIF.Core;
+
+namespace UnityGIF.Data
+{
+	/// <summary>
+	/// Texture + delay + disposal method
+	/// </summary>
+	public class GifFrame
+	{
+		public Texture2D Texture;
+		public float Delay;
+		public DisposalMethod DisposalMethod = DisposalMethod.RestoreToBackgroundColor;
+
+		public void ApplyPalette(MasterPalette palette)
+		{
+			TextureConverter.ConvertTo8Bits(ref Texture, palette);
+		}
+	}
+}
