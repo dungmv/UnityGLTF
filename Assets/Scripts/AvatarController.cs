00e6ee4f32de7b328776bfc01e40bb9e4f9152b3
dungmv <maivandung91@gmail.com>
2023-05-28 00:28:03 +0700

    update api

---
+++ Assets/Scripts/AvatarController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AvatarController : MonoBehaviour
+{
+    public TMP_Text textName;
+    public TMP_Text textStatus;
+    public GameObject avatar;
+}
--- Assets/Scripts/ModelViewer.cs
+++ Assets/Scripts/ModelViewer.cs
@@ -1,23 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Collections.Generic;
 using ReadyPlayerMe.AvatarLoader;
 using UnityEngine.Networking;
 using GLTFast;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
 using System.Runtime.InteropServices;
+using Newtonsoft.Json;
 
 // sample avatar
 // Feminine https://models.readyplayer.me/645938677cf7d03f60e0b4e3.glb /1.73628
 // Masculine https://models.readyplayer.me/6423ac9aa9cf14ab7e456f88.glb /1.86452
 public class ModelViewer : MonoBehaviour
 {
-    [SerializeField] private AnimationClip[] clips;
+    public class LoadAvatarData
+    {
+        public string id;
+        public string name;
+        public string status;
+        public string url;
+        public Vector3 position;
+    }
+    public class RunAnimationData
+    {
+        public string id;
+        public int animationId;
+    }
 
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private AnimationClip[] clips;
+    [SerializeField] private AvatarController avatarPrefab;
+
+    private Dictionary<string, AvatarController> avatarDict;
     private PlayableGraph playableGraph;
-    private Animator animator;
-#if UNITY_IOS || UNITY_TVOS
+    static private Quaternion defaultRotation = Quaternion.Euler(0, 180, 0); 
+#if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
     public static extern void onAvatarLoadCompleted(string url);
     [DllImport("__Internal")]
@@ -26,10 +45,20 @@ public class ModelViewer : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        avatarDict = new Dictionary<string, AvatarController>();
         playableGraph = PlayableGraph.Create();
-        // Load("https://models.readyplayer.me/6423ac9aa9cf14ab7e456f88.glb");
-        // Load("https://models.readyplayer.me/645938677cf7d03f60e0b4e3.glb");
-#if UNITY_IOS || UNITY_TVOS
+#if UNITY_EDITOR
+        StartCoroutine(DownloadAvatar(
+            "6423ac9aa9cf14ab7e456f88",
+            "https://models.readyplayer.me/6423ac9aa9cf14ab7e456f88.glb",
+            "dungmv",
+            "happy",
+            new Vector3(0, 0, 0)
+        ));
+
+        SetBackgroundColor("#00FF00");
+#endif
+#if UNITY_IOS && !UNITY_EDITOR
         onInitialized();
 #endif
     }
@@ -45,12 +74,8 @@ public class ModelViewer : MonoBehaviour
 
     //}
 
-    public void Load(string url)
-	{
-        StartCoroutine(DownloadAvatar(url));
-    }
 
-    private IEnumerator DownloadAvatar(string url)
+    private IEnumerator DownloadAvatar(string avatarId, string url, string avatarName, string avatarStatus, Vector3 position)
     {
         using (var webRequest = UnityWebRequest.Get(url))
         {
@@ -61,43 +86,53 @@ public class ModelViewer : MonoBehaviour
                 Debug.LogError($"Error {webRequest.responseCode} - {webRequest.error}");
                 yield break;
             }
-            ImportAvatar(webRequest.downloadHandler.data, url);
+            ImportAvatar(webRequest.downloadHandler.data, avatarId, avatarName, avatarStatus, position);
         }
     }
 
-    private async void ImportAvatar(byte[] bytes, string url)
+    private async void ImportAvatar(byte[] bytes, string avatarId, string avatarName, string avatarStatus, Vector3 position)
     {
         GltfImport gltf = new GltfImport();
-        if (await gltf.LoadGltfBinary(bytes, new Uri(url)))
+        if (await gltf.LoadGltfBinary(bytes))
         {
-            GameObject avatar = new GameObject("Avatar");
+            var go = Instantiate(avatarPrefab);
+            GameObject avatar = new GameObject(avatarId, new Type[] { typeof (Animator) });
+            go.transform.position = position;
+            go.avatar = avatar;
+            avatar.transform.parent = go.transform;
+            avatar.transform.position = Vector3.zero;
+            avatar.transform.rotation = defaultRotation;
             avatar.SetActive(true);
             GltFastGameObjectInstantiator customInstantiator = new GltFastGameObjectInstantiator(gltf, avatar.transform);
             await gltf.InstantiateMainSceneAsync(customInstantiator);
-            SetupAnimator(avatar);
-            
+
+            // setup animator
+            var animator = avatar.GetComponent<Animator>();
+            animator.avatar = AnimatorFromAvatar(avatar);
+            animator.applyRootMotion = true;
+
+            // setup name and status
+            go.textName.text = avatarName;
+            go.textStatus.text = avatarStatus;
+            // save
+            avatarDict[avatarId] = go;
         }
-#if UNITY_IOS || UNITY_TVOS
-        onAvatarLoadCompleted(url);
+#if UNITY_IOS && !UNITY_EDITOR
+        onAvatarLoadCompleted(avatarName);
 #endif
     }
 
-    private void SetupAnimator(GameObject model)
+    private Avatar AnimatorFromAvatar(GameObject model)
     {
         // val.avatar = AvatarBuilder.BuildHumanAvatar(model, new HumanDescription()
         // {
         //     human = humanBones,
         // });
         var gender = DetectGender(model);
-        string text = ((gender == OutfitGender.Masculine) ? "AnimationAvatars/MasculineAnimationAvatar" : "AnimationAvatars/FeminineAnimationAvatar");
-        Animator val = model.AddComponent<Animator>();
-        val.avatar = Resources.Load<Avatar>(text);
-        val.avatar.name = "avatar";
-        val.applyRootMotion = true;
-
-        // setup animator
-        animator = model.GetComponent<Animator>();
-        animator.applyRootMotion = false;
+        string text = ((gender == OutfitGender.Masculine) ?
+            "AnimationAvatars/MasculineAnimationAvatar" :
+            "AnimationAvatars/FeminineAnimationAvatar");
+        return Resources.Load<Avatar>(text);
     }
 
     private OutfitGender DetectGender(GameObject avatar)
@@ -107,18 +142,56 @@ public class ModelViewer : MonoBehaviour
         return headTop.position.y > 1.8f ? OutfitGender.Masculine : OutfitGender.Feminine;
     }
 
-    public void RunAnimation(string msg)
+    public void LoadAvatar(string json)
     {
-        int id = Int32.Parse(msg);
-        var clip = clips[id];
-        var playableOutput = AnimationPlayableOutput.Create(playableGraph, "AnimationClip", animator);
-        // Wrap the clip in a playable
-        clip.wrapMode = WrapMode.Loop;
-        var playableClip = AnimationClipPlayable.Create(playableGraph, clip);
-        // Connect the Playable to an output
-        playableOutput.SetSourcePlayable(playableClip);
-        // Plays the Graph.
-        playableGraph.Play();
+        var data = JsonUtility.FromJson<LoadAvatarData>(json);
+        StartCoroutine(DownloadAvatar(data.id, data.url, data.name, data.status, data.position));
+    }
+
+    public void SetPositionAvatar(string json)
+    {
+        var data = JsonUtility.FromJson<LoadAvatarData>(json);
+        if (avatarDict.TryGetValue(data.id, out var avatar))
+        {
+            avatar.transform.position = data.position;
+        }
+    }
+
+    public void RunAnimation(string json)
+    {
+        var data = JsonUtility.FromJson<RunAnimationData>(json);
+        if (avatarDict.TryGetValue(data.id, out var avatar))
+        {
+            var clip = clips[data.animationId];
+            var animator = avatar.avatar.GetComponent<Animator>();
+            var playableOutput = AnimationPlayableOutput.Create(playableGraph, "AnimationClip", animator);
+            // Wrap the clip in a playable
+            clip.wrapMode = WrapMode.Loop;
+            var playableClip = AnimationClipPlayable.Create(playableGraph, clip);
+            // Connect the Playable to an output
+            playableOutput.SetSourcePlayable(playableClip);
+            // Plays the Graph.
+            playableGraph.Play();
+        }
+    }
+
+    public void SetPositionCamera(string json)
+    {
+        var newPos = JsonUtility.FromJson<Vector3>(json);
+        mainCamera.transform.position = newPos;
+    }
+
+    public void SetBackgroundColor(string color)
+    {
+        if (ColorUtility.TryParseHtmlString(color, out var val))
+        {
+            mainCamera.backgroundColor = val;
+        }
+    }
+
+    public void SetZoom(float zoom)
+    {
+        
     }
 }
 
